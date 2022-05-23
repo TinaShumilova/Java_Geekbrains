67cd353918045d9a9e1f75005de2d45d45a384bc
Tina Shumilova <shutiva@gmail.com>
2022-05-23 19:10:35 +0300

    Task2 PutOutFile complete

--- program2.java
+++ program2.java
@@ -1,10 +1,21 @@
 package java_projects;
+import java.io.*;
 
 /**
  * Даны два файла, в каждом из которых находится запись многочлена. 
  * Сформировать файл содержащий сумму многочленов.
  */
 public class program2 {
+    public static void main(String[] args) throws Exception {
+        String fileFirst = PutOutFile("first.txt");
+        String fileSecond = PutOutFile("second.txt");
+    }
 
+    static String PutOutFile(String fileName) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String text = br.readLine();
+        System.out.printf("%s\n", text);
+        return text;
+    }
     
 }
